35287a33cf67bcb1dcd077e3571fd4b5e20c2ee3
Raúl Pardo Zurita <raul.pardoz@gmail.com>
2022-07-05 22:52:25 -0400

    Modifcación script ejercicio 3
    Se corrige bug que hacía que se asignara un color automaticamente al div no clickeado

--- assets/js/3-script.js
+++ assets/js/3-script.js
@@ -2,10 +2,10 @@ function pintar(element, color = "green") {
     element.style.backgroundColor = color
 }
 
-
 ele = document.getElementById("ele1")
-color = "yellow"
-ele.addEventListener("click", pintar(ele, color))
-
+let color = "yellow"
+ele.addEventListener("click", () => {
+    pintar(ele, color)
+})
 
 
